443ae60abb08290f5632ecd145e56f962f26d464
Tanvir Ahmed <tanvirpoly@gmail.com>
2022-12-31 02:25:59 +0600

    includes

---
+++ includes/footer.php
@@ -0,0 +1,121 @@
+<footer id="footer" class="footer color-bg">
+ <div class="links-social inner-top-sm">
+  <div class="container">
+    <div class="row">
+     <div class="col-xs-12 col-sm-6 col-md-4">
+      <!-- ================ CONTACT INFO ===================== -->
+      <div class="contact-info">
+        <div class="footer-logo">
+          <div class="logo">
+            <a href="index.php">
+
+              <h3>Online Food Order</h3>
+            </a>
+          </div><!-- /.logo -->
+
+        </div><!-- /.footer-logo -->
+
+        <div class="module-body m-t-20">
+          <p class="about-us"> A Powerful online ordering system for restaurants to get online and start selling to customers directly.</p>
+
+          <div class="social-icons">
+
+            <a href="https://www.facebook.com/tanvir.hard/" target="_blank" class=' '><i class="icon fa fa-facebook"></i></a>
+            <a href="https://twitter.com/px_tanvir" target="_blank"><i class="icon fa fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/tanvirx/" target="_blank"><i class="icon fa fa-linkedin"></i></a>
+            <a href="https://www.instagram.com/tanvir.xp/" target="_blank"><i class="icon fa fa-instagram"></i></a>
+            <a href="https://www.pinterest.com/" target="_blank"><i class="icon fa fa-pinterest"></i></a>
+
+          </div><!-- /.social-icons -->
+        </div><!-- /.module-body -->
+
+      </div><!-- /.contact-info -->
+      <!-- ====================== CONTACT INFO : END =========================== -->            	
+    </div><!-- /.col -->
+
+    <div class="col-xs-12 col-sm-6 col-md-4">
+     <!-- ======================== CONTACT TIMING================ -->
+     <div class="contact-timing">
+       <div class="module-heading" style=" padding: 17px; width: 100px; margin-left: 125px;">
+       <!--  <button type="button" class="btn btn-success">Open</button> -->
+
+      </div><!-- /.module-heading -->
+
+        <!--   <div class="module-body outer-top-xs">
+              <div class="table-responsive">
+               <table class="table">
+                <tbody>
+                 <tr><td>Monday-Friday:</td><td class="pull-right">08.00 To 18.00</td></tr>
+                 <tr><td>Saturday:</td><td class="pull-right">09.00 To 20.00</td></tr>
+                 <tr><td>Sunday:</td><td class="pull-right">10.00 To 20.00</td></tr>
+             </tbody>
+         </table>
+     </div>
+   </div> -->
+ </div><!-- /.contact-timing -->
+ <!-- ==================== CONTACT TIMING : END ============================ -->   
+</div><!-- /.col -->
+
+<div class="col-xs-12 col-sm-6 col-md-4">
+ <!-- ========================== INFORMATION=================== -->
+ <div class="contact-information">
+   <div class="module-heading">
+    <h4 class="module-title">information</h4>
+  </div><!-- /.module-heading -->
+
+  <div class="module-body outer-top-xs">
+    <ul class="toggle-footer" style="">
+      <li class="media">
+        <div class="pull-left">
+         <span class="icon fa-stack fa-lg">
+          <i class="fa fa-circle fa-stack-2x"></i>
+          <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+        </span>
+      </div>
+      <div class="media-body">
+
+        <span><a href="https://www.google.com/maps/place/West+Shewrapara,+Dhaka/@23.7899915,90.3686803,16z/data=!3m1!4b1!4m5!3m4!1s0x3755c0b58914ed45:0xb88612d8fbfb11a9!8m2!3d23.7881199!4d90.3736584" target="_blank">Shewrapara-Mirpur, Dhaka</a></span>
+      </div>
+    </li>
+
+    <li class="media">
+      <div class="pull-left">
+       <span class="icon fa-stack fa-lg">
+        <i class="fa fa-circle fa-stack-2x"></i>
+        <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+      </span>
+    </div>
+    <div class="media-body">
+
+      <p> 01753781569 </p>
+    </div>
+  </li>
+
+  <li class="media">
+    <div class="pull-left">
+     <span class="icon fa-stack fa-lg">
+      <i class="fa fa-circle fa-stack-2x"></i>
+      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+    </span>
+  </div>
+  <div class="media-body">
+
+    <span><a href="index.php">tanvirpoly@gmail.com</a></span>
+  </div>
+</li>
+
+</ul>
+</div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ======================= INFORMATION : END ================================== -->            	
+</div><!-- /.col -->
+</div><!-- /.row -->
+</div><!-- /.container -->
+</div><!-- /.links-social -->
+ 
+    <div class="container">
+      <b class="copyright">&copy; 2021 Online Food Delivery </b> All rights reserved.
+    </div>
+    <br> <br>
+    <br> 
+ 
---
+++ includes/google_ads_slider.php
@@ -0,0 +1,79 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+	<h3 class="section-title">Google Ads</h3>
+	<div class="logo-slider-inner">	
+		<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+
+			<div class="item">
+				<a href="https://www.foodpanda.com.bd/" target="_blank" class="image">
+					<img data-echo="google_ads_image/foodPanda.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+
+			<div class="item">
+				<a href="https://www.shwapno.com/" target="_blank" class="image">
+					<img data-echo="google_ads_image/Shwapno.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+
+			<div class="item">
+				<a href="https://agorasuperstores.com/" target="_blank" class="image"> 
+					<img data-echo="google_ads_image/agora.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+
+			<div class="item">
+				<a href="https://chaldal.com/" target="_blank" class="image">
+					<img data-echo="google_ads_image/chaldal.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div><!--/.item-->
+
+			<div class="item">
+				<a href="https://mithaibd.com/" target="_blank" class="image">
+					<img data-echo="google_ads_image/Mithai.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div><!--/.item-->
+
+			<div class="item">
+				<a href="https://tastytreatbd.com/" target="_blank" class="image">
+					<img data-echo="google_ads_image/Testy.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+
+			
+			<div class="item">
+				<a href="https://evaly.com.bd/express/express-bullet-food" target="_blank" class="image">
+					<img data-echo="google_ads_image/Efood.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+			<div class="item">
+				<a href="https://evaly.com.bd/" target="_blank" class="image">
+					<img data-echo="google_ads_image/Evaly.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+			<div class="item">
+				<a href="https://www.daraz.com.bd/" target="_blank" class="image">
+					<img data-echo="google_ads_image/daraz.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+				<div class="item">
+				<a href="https://www.foodpanda.com.bd/" target="_blank" class="image">
+					<img data-echo="google_ads_image/foodPanda.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+
+			<div class="item">
+				<a href="https://www.shwapno.com/" target="_blank" class="image">
+					<img data-echo="google_ads_image/Shwapno.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+
+			<div class="item">
+				<a href="https://agorasuperstores.com/" target="_blank" class="image"> 
+					<img data-echo="google_ads_image/agora.jpg" src="assets/images/blank.gif" alt="Visit!">
+				</a>	
+			</div>
+
+		</div><!-- /.owl-carousel #logo-slider -->
+	</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
