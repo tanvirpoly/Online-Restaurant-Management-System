060582f0cc083f96430eb84deb0c5ea158a668bf
Tanvir Ahmed <tanvirpoly@gmail.com>
2023-01-27 16:23:44 +0600

    footer

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,6 +1,6 @@
 	<div class="footer">
 		<div class="container">
 
-			<b class="copyright">&copy; 2021 Food Delivery </b> All rights reserved.
+			<b class="copyright">&copy; 2023 Food Delivery </b> All rights reserved.
 		</div>
 	</div>
